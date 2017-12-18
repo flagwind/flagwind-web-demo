a4280144cf329f0f380961e8c21712f3619686c9
jason <jasonsoop@gmail.com>
2017-12-18 15:23:35 +0800

    加入数字计数组件的调试代码。

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h1>Dashboard...</h1>
+        <fw-number-counter style="font-size:48px;" :value="value"></fw-number-counter>
+        <br />
+        <i-button @click="value += 10">加值</i-button>
+        <i-button @click="value -= 10">减值</i-button>
     </div>
 </template>
 
@@ -14,6 +17,6 @@ import { component, View } from "flagwind-web";
 @component
 export default class Dashboard extends View
 {
-    
+    protected value: number = 123456;
 }
 </script>
